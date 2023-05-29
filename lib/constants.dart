57046f764d4fccd06a8822f034b16449068a49a4
AkritiSaraogi <akritis@byteridge.com>
2023-05-29 15:54:02 +0530

    api integration for play integrity

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  static String baseUrl =
+      'https://bbed-2401-4900-5fc1-97c6-b922-52ce-70be-e3c9.ngrok-free.app';
+  static String usersEndpoint = '/app/verifyAppCheckToken';
+  static String authorization = "Authorization";
+  static String bearer = "Bearer";
+  static const String IS_FIREBASE_APPCHECK_TOKEN_CALLED =
+      'FIREBASE_APPCHECK_TOKEN_CALLED';
+  static const String IS_VERIFIED = 'ISVERIFIED';
+}
---
+++ lib/custom_dialog.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomDialog {
+  static void showJailBrokenAlertDialog(
+    BuildContext context,
+    String title,
+    String content,
+  ) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 20.0,
+              color: Colors.black,
+            ),
+          ),
+          content: Text(
+            content,
+            style: const TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 14.0,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          actions: <Widget>[
+            Center(
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: () {
+                    Platform.isAndroid
+                        ? SystemChannels.platform
+                            .invokeMethod<void>('SystemNavigator.pop')
+                        : exit(0);
+                  },
+                  child: const Text(
+                    'OK',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.0,
+                      color: Colors.white,
+                    ),
+                  )),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
